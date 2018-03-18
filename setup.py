5ea24a9f9b5c30b46e18031e3d6e7ad671a1caeb
Riley Flynn <riley@rileyflynn.me>
2018-03-18 13:39:04 -0230

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="pyrichpresence",
+    version="1.0",
+    packages=["pyrichpresence"],
+    description="Python library providing easy access to Discord's rich presence functionality",
+    author="nint8835, ry00001, AlexFlipnote"
+)
